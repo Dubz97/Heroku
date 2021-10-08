557acdd16a7c0a1d70a4cf0d1b3989fb92805d5a
Christoffer <christofferp97@hotmail.com>
2021-10-08 13:53:22 +0200

    Heroku

---
+++ src/main/java/com/example/heroku/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.heroku;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
+  @GetMapping("/test")
+  public String test() {
+    return "test";
+  }
+}
